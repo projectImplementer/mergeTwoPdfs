8baa8c737a6ee16258fd31e4600b576e32aa997b
projectImplementer <bogdanm.demetrescu@gmail.com>
2020-10-21 23:49:14 +0300

    First commit v1.0.0

---
+++ pdf.py
@@ -0,0 +1,16 @@
+import sys
+import PyPDF2
+
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+
+wtr = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(wtr.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
